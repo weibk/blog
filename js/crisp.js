e4ac3dcbe866e16f78e02f7e6cb12087cdf57109
weibk <79395818+weibk@users.noreply.github.com>
2021-04-26 15:42:08 +0000

    Deploying to gh-pages from @ weibk/blog@57f9cf4b2f39a168b4cc156e9e812502ebf7d212 🚀

---
+++ js/crisp.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+	window.$crisp=[];
+	window.CRISP_WEBSITE_ID="d55b1b42-cb4d-4e52-b553-68f0ef6fb320";
+	(function(){d=document;s=d.createElement("script");
+	s.src="https://client.crisp.chat/l.js";
+	s.async=1;
+	d.getElementsByTagName("head")[0].appendChild(s);})();
+	console.log(111);
+}
+
+
